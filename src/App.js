79551e389cd545d34782f7fedcaa913750c337ca
sathya4555 <62885194+sathya4555@users.noreply.github.com>
2022-03-07 16:45:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import socketIOClient from "socket.io-client";
 import SendData from "./components/SendData";
 import Socket from "./components/Socket";
-const ENDPOINT = "http://18.141.166.49:3003";
+const ENDPOINT = "ws://18.141.166.49:3003";
 
 function App() {
   const [response, setResponse] = useState("");
